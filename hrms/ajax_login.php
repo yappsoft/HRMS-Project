45e41287e1c30587d5cdd0c33bd4d8adabe9f446
Yajveer <yappsoft@gmail.com>
2016-05-12 18:46:39 +0530

    add changes

--- hrms/ajax_login.php
+++ hrms/ajax_login.php
@@ -3,12 +3,9 @@ include("dbcon.php");
 if(isset($_POST['email']) && isset($_POST['password']))
 {
 // email and password sent from Form
-$email = $_POST['email']; 
-//Here converting passsword into MD5 encryption. 
-$password = $_POST['password']; 
-
-
-
+$email = $_POST['email'];
+//Here converting passsword into MD5 encryption.
+$password = $_POST['password'];
 $result=mysqli_query($con,"SELECT * FROM login_tbl WHERE email='$email' and password='$password'");
 $count=mysqli_num_rows($result);
 $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -17,24 +14,24 @@ if($count > 0){
 $type = $row['user_type'];
 if($type == 'employee'){
     echo 'emp';
-    
+
 }else if($type == 'superadmin'){
-    
+
     echo 'spradmin';
 }else if($type == 'admin'){
 echo 'admin';
-    
-    
+
+
 }
 
 
 
 }else{
-    
+
     echo 'cloud not find email and password';
 }
 
 }else{
-    
+
     echo 'server side validation error';
 }
