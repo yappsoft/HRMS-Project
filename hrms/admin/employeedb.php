1396dfb0416502bb9262a7f2e9ccf3609f8718f4
yajveer <1983kamlesh@gmail.com>
2016-05-16 10:27:43 +0530

    changes fixed

--- hrms/admin/employeedb.php
+++ hrms/admin/employeedb.php
@@ -22,11 +22,14 @@ $bank_branch_name2=$_POST['bank_branch_name'];
 $number_of_leaves2=$_POST['Numbers_Of_Leave'];
 $com_emp_id2=$_POST['employee_id'];
 $department2=$_POST['department'];
+$image2 = '';
 
 
 //Insert query
 
-$query = mysqli_query($con,"insert into employee_tbl(company_id,com_emp_id,first_name,last_name, email, password, education, date_of_birth, mobile_no, alternate_no, family_no, local_address, permanent_address  , account_no, ifsc_code, date_of_joining, bank_name, bank_branch_name, number_of_leaves,department) values ('$company_id','$com_emp_id2','$first_name2','$last_name2','$email2','$password2','$education2','$date_of_birth2','$mobile_no2','$alternate_no2','$family_no2','$local_address2','$permanent_address2','$account_no2','$ifsc_code2','$date_of_joining2','$bank_name2','$bank_branch_name2','$number_of_leaves2','$department2')");
+$query = mysqli_query($con,"insert into employee_tbl(company_id,com_emp_id,first_name,last_name, email, password, education, date_of_birth, mobile_no, alternate_no, family_no, local_address, permanent_address, image, account_no, ifsc_code, date_of_joining, bank_name, bank_branch_name, number_of_leaves,department) values ('$company_id','$com_emp_id2','$first_name2','$last_name2','$email2','$password2','$education2','$date_of_birth2','$mobile_no2','$alternate_no2','$family_no2','$local_address2','$permanent_address2','$image2','$account_no2','$ifsc_code2','$date_of_joining2','$bank_name2','$bank_branch_name2','$number_of_leaves2','$department2')");
+echo $query;
+die();
 $id_emp = $con->insert_id;
 if($query){
 $q1="insert into login_tbl(employee_id,email,password,company_id,login_status,user_type)values('$id_emp','$email2','$password2','$company_id','active','employee')";
